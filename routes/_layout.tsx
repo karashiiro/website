c6a9210af29718c08fff70025ceeb15ad5b0d150
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-09-17 10:51:59 -0700

    Use centered layout with embroidery

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,11 +1,9 @@
 import { LayoutProps } from "$fresh/server.ts";
 import Container from "../components/Container.tsx";
-import Header from "../components/Header.tsx";
 
 export default function Layout({ Component }: LayoutProps) {
   return (
     <>
-      <Header />
       <main class="mx-auto max-w-screen-xl">
         <Container>
           <Component />
