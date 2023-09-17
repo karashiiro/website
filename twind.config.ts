ac1213b2303f972f68fe8477ccb16fbc936a7a5a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-09-17 08:51:17 -0700

    Upgrade Twind and add some theme stuff

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,21 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { defineConfig, Preset } from "https://esm.sh/@twind/core@1.1.3";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
+import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 
 export default {
+  ...defineConfig({
+    presets: [presetTailwind() as Preset, presetAutoprefix()],
+    theme: {
+      extend: {
+        animation: {
+          "spin-slow": "spin 90s linear infinite",
+        },
+        backgroundImage: {
+          "profile":
+            "url('https://avatars.githubusercontent.com/u/49822414?v=4')",
+        },
+      },
+    },
+  }),
   selfURL: import.meta.url,
-} as Options;
+};
