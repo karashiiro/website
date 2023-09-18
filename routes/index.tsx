be581d3521b015e33e93148373b358e6b3ee9cdf
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-09-18 12:43:26 -0700

    Make social buttons fit with theme

--- routes/index.tsx
+++ routes/index.tsx
@@ -29,13 +29,29 @@ ButtonLinkList.Item = (
 function MyLinks() {
   return (
     <nav class="m-auto md:ml-4 max-w-xs md:max-w-sm divide-dashed divide-y-2">
-      <div class="flex flex-row">
-        <a href="https://github.com/karashiiro" tabIndex={0}>
-          <IconBrandGithub class="text-slate-400 hover:text-slate-700 duration-100" />
-        </a>
-        <a href="https://twitter.com/karashiiro1" tabIndex={0}>
-          <IconBrandTwitter class="text-slate-400 hover:text-slate-700 duration-100" />
-        </a>
+      <div class="flex flex-row font-bold text-sm text-blue-900 text-center">
+        <div class="mr-1 flex rounded-full shadow-md hover:shadow-lg duration-100">
+          <a
+            class="rounded-full p-1 bg-blue-300"
+            href="https://github.com/karashiiro"
+            tabIndex={0}
+          >
+            <div class="rounded-full p-1 border-dashed border-2 border-white">
+              <IconBrandGithub />
+            </div>
+          </a>
+        </div>
+        <div class="mr-1 flex rounded-full shadow-md hover:shadow-lg duration-100">
+          <a
+            class="rounded-full p-1 bg-blue-300"
+            href="https://twitter.com/karashiiro1"
+            tabIndex={0}
+          >
+            <div class="rounded-full p-1 border-dashed border-2 border-white">
+              <IconBrandTwitter />
+            </div>
+          </a>
+        </div>
       </div>
     </nav>
   );
