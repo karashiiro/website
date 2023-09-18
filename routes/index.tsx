292506db6804b73318f70b55d8f176c6bcabd35c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-09-17 22:48:38 -0700

    Make layout wider on desktop

--- routes/index.tsx
+++ routes/index.tsx
@@ -28,7 +28,7 @@ ButtonLinkList.Item = ({ href, gh, children }: {
 
 function MyLinks() {
   return (
-    <nav class="m-auto md:ml-4 max-w-xs divide-dashed divide-y-2">
+    <nav class="m-auto md:ml-4 max-w-xs md:max-w-sm divide-dashed divide-y-2">
       <div class="mb-1">
         <h2 class="text-xl font-fancy font-semibold">Find me on</h2>
         <ButtonLinkList>
@@ -62,16 +62,16 @@ function MyLinks() {
             PSO2 Patch File Viewer
           </ButtonLinkList.Item>
           <ButtonLinkList.Item
-            href="https://github.com/the-convocation/twitter-scraper"
-            gh="https://github.com/the-convocation/twitter-scraper"
-          >
-            @the-convocation/twitter-scraper
-          </ButtonLinkList.Item>
-          <ButtonLinkList.Item
             href="https://github.com/karashiiro/xiv-resources"
             gh="https://github.com/karashiiro/xiv-resources"
           >
             xiv-resources
+          </ButtonLinkList.Item>
+          <ButtonLinkList.Item
+            href="https://github.com/the-convocation/twitter-scraper"
+            gh="https://github.com/the-convocation/twitter-scraper"
+          >
+            @the-convocation/twitter-scraper
           </ButtonLinkList.Item>
         </ButtonLinkList>
       </div>
