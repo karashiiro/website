865c4f3a91f0162a2c3f5060afc17a1a0148ed1f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-09-18 22:40:17 -0700

    Remove unused code

--- components/ButtonLink.tsx
+++ components/ButtonLink.tsx
@@ -1,12 +1,5 @@
 import { ComponentChildren } from "preact";
 
-const variants = {
-  blue: "bg-blue-300 text-blue-900",
-  pink: "bg-pink-300 text-pink-900",
-};
-
-const variantDefault = "blue";
-
 const shapes = {
   pill: "px-2 py-1",
   circle: "p-1 aspect-square",
@@ -15,9 +8,8 @@ const shapes = {
 const shapeDefault = "pill";
 
 export function ButtonLink(
-  { href, variant, shape, children }: {
+  { href, shape, children }: {
     href: string;
-    variant?: keyof typeof variants;
     shape?: keyof typeof shapes;
     children: ComponentChildren;
   },
@@ -25,7 +17,7 @@ export function ButtonLink(
   return (
     <div class="flex z-10 rounded-full shadow-md hover:shadow-lg duration-100 font-bold text-sm text-center">
       <a
-        class={`rounded-full p-1 ${variants[variant ?? variantDefault]}`}
+        class="rounded-full p-1 bg-blue-300 text-blue-900"
         tabIndex={0}
         href={href}
       >
